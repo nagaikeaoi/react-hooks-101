c6d86636c4190e1039828a116fef9cf05c6e7531
aoi nagaike <nagaike_aoi@beanslabo.co.jp>
2023-11-16 18:07:27 +0900

    セクション3-15:イベント一括削除機能、ボタンの有効無効の切り換え、削除ボタンの確認アラートを追加

--- src/components/App.js
+++ src/components/App.js
@@ -22,6 +22,16 @@ const App = () => {
     setBody("");
   };
 
+  const deleteAllEvents = (e) => {
+    e.preventDefault();
+    const result = window.confirm(
+      "すべてのイベントを本当に削除しても良いですか？"
+    );
+    if (result) dispatch({ type: "DELETE_ALL_EVENTS" });
+  };
+
+  const unCreatable = title === "" || body === "";
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -46,10 +56,20 @@ const App = () => {
           ></textarea>
         </div>
 
-        <button className="btn btn-primary" onClick={addEvent}>
+        <button
+          className="btn btn-primary"
+          onClick={addEvent}
+          disabled={unCreatable}
+        >
           イベントを作成する
         </button>
-        <button className="btn btn-danger">すべてのイベントを削除する</button>
+        <button
+          className="btn btn-danger"
+          onClick={deleteAllEvents}
+          disabled={state.length === 0}
+        >
+          すべてのイベントを削除する
+        </button>
       </form>
 
       <h4>イベント一覧</h4>
