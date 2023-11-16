e10bc5502d8f3efcfdb9a5dabc58f40d38bb5dc2
aoi nagaike <nagaike_aoi@beanslabo.co.jp>
2023-11-16 12:23:45 +0900

    セクション2-9:useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from "react";
 
 const App = (props) => {
   const [state, setState] = useState(props);
   const { name, price } = state;
 
+  useEffect(() => {
+    console.log("This is like componentDidMount or componentDidUpdate.");
+  });
+
+  // レンダリングを一度だけにしたい場合
+  useEffect(() => {
+    console.log("This is like componentDidMount.");
+  }, []);
+
+  // 限定的な時だけ呼び出す(nameの処理のときだけ)
+  useEffect(() => {
+    console.log("This callback is for name only.");
+  }, [name]);
+
   return (
     <>
-      <p>現在の{name}は、{price}円です。</p>
-      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
-      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
+      <p>
+        現在の{name}は、{price}円です。
+      </p>
+      <button onClick={() => setState({ ...state, price: price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>
+        -1
+      </button>
       <button onClick={() => setState(props)}>reset</button>
-      <input value={name} onChange={e => setState({...state, name: e.target.value})}/>
+      <input
+        value={name}
+        onChange={(e) => setState({ ...state, name: e.target.value })}
+      />
     </>
   );
-}
+};
 
 App.defaultProps = {
-    name: '',
-    price: 1000
-}
+  name: "",
+  price: 1000,
+};
 
 export default App;
